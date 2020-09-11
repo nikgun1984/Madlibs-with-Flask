332322920d18d616994c40b30f8c79ad73cdfdd0
Nick Gundobin <ngundobin@gmail.com>
2020-09-11 11:46:05 -0400

    css/routes/html were added

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = 'whateverpassword'
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+@app.route('/')
+def madlibs_form():
+    return render_template('form.html')
+
+@app.route('/your_story')
+def your_story():
+    place = request.args.get("place")
+    noun = request.args.get("noun")
+    verb = request.args.get("verb")
+    adj = request.args.get("adjective")
+    plur = request.args.get("plural_noun")
+    ans = {
+        "place":place,
+        "noun":noun,
+        "verb":verb,
+        "adjective":adj,
+        "plural_noun": plur
+    }
+    text = story.generate(ans)
+    print(text)
+    return render_template('story.html',story=text)
